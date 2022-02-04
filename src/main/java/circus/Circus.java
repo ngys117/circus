1ccc1310072ab96f9f933c224a6e596085882335
ngys117 <e0540880@u.nus.edu>
2022-02-04 19:03:06 +0900

    Update code according to lecture instructions

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,3 +1,8 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Duck;
+
 public class Circus {
     private static Animal[] animals = {
             new Duck(),
@@ -16,25 +21,26 @@ public class Circus {
         }
     }
 
-    private static int calculateValue(Equipment[] equipments) {
+    private static int calculateValue(Asset[] assets) {
         int total = 0;
-        for (Equipment e : equipments) {
-            if (e.getValue() <= 5) {
-                System.out.println("Ignoring low value item: " + e.getValue());
-            } else {
-                total += e.getValue();
-                System.out.println("Adding item value: " + e.getValue());
-                // some 
-                // more
-                // code 
-                // here ...
+        for (Asset asset : assets) {
+            if (asset.getValue() <= 5) {
+                System.out.println("Ignoring low value item: " + asset.getValue());
+                continue;
             }
+            total += asset.getValue();
+            System.out.println("Adding item value: " + asset.getValue());
+            // some
+            // more
+            // code
+            // here ...
         }
         return total;
     }
 
     public static void main(String[] args) {
         makeAnimalsTalk();
+        System.out.println("Total value of animals " + calculateValue(animals));
         System.out.println("Total value of equipments " + calculateValue(equipments));
     }
 }
